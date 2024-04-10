8117bd82c400e36e547befa3eec117c8a3baa940
Swati <jswati603@gmail.com>
2024-04-10 12:32:27 +0530

    login

--- routes/User.js
+++ routes/User.js
@@ -44,6 +44,38 @@ authRouter.post("/signup" ,async(req,res)=>{
 
 })
 
+
+authRouter.post("/login", async (req,res)=>{
+    const {email,password}=req.body
+
+    if(!email||!password){
+        return custumResponse(res,400,false,"Please fill all the fields,null")
+    }
+
+    try{
+        const foundUser=await User.findOne({email:email})
+        if (foundUser==null){
+            return custumResponse(res,400,false,"User doesnot exist",null)
+        }
+         console.log("1", foundUser)
+        //check hashed password
+
+        const isMatch=await bcrypt.compare(password,foundUser.password)
+        console.log("2", isMatch, typeof isMatch)
+        if(isMatch==true){
+            console.log("3", "line 66")
+            return custumResponse(res,200,true,"User logged in successfully", foundUser)
+        }
+
+        else{
+            return custumResponse(res,400,false,"Invalid credentials",null)
+        }
+    }
+    catch(err){
+        custumResponse(res,500,false,"something went wrong",null)
+    }
+})
+
 export default authRouter
 
 
